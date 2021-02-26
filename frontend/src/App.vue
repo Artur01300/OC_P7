94474a015c24f9bf1f157b7e374927160b2e8680
Artur01300 <artur.karapetyan@outlook.fr>
2021-02-26 08:09:49 +0100

    Vue router installé

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,19 +1,14 @@
 <template>
-  <h1>Hello World</h1>
-  <!-- <img alt="Vue logo" src="./assets/logo.png"/> -->
-  <!-- <HelloWorld msg="Welcome to Youue"/> -->
+  <div id="app">
+    
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
+
+  </div>
 </template>
-
-<script>
-// import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    // HelloWorld
-  }
-}
-</script>
 
 <style>
 #app {
@@ -22,6 +17,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
